a2d116321c06314cc40b51b16f2b98619e37985d
Joseph A. Friend <josephangelfriend1@gmail.com>
2026-03-09 04:19:12 -0700

    Added locked/unlock levels in UI

--- scripts/level_card.gd
+++ scripts/level_card.gd
@@ -3,12 +3,13 @@ extends PanelContainer
 @onready var level_label: Label = $MarginContainer/VBoxContainer/HBoxContainer/Level
 @onready var grid_label: Label = $MarginContainer/VBoxContainer/HBoxContainer/Grid
 @onready var safety_label: Label = $MarginContainer/VBoxContainer/Safety
-
+@onready var play_button: Button = $MarginContainer/VBoxContainer/Button
 var index: int = 0
 signal selected(idx: int)
 
 func setup(level: int, level_info: LevelInfo):
 	self.index = level
+	play_button.disabled = true
 	level_label.text = "Level: %d" % [level + 1]
 	grid_label.text = "Grid: %dx%d" % [level_info.x, level_info.y]
 	var safety_string: String = "Safety: "
@@ -18,6 +19,8 @@ func setup(level: int, level_info: LevelInfo):
 		safety_string += "Nay"
 	safety_label.text = safety_string
 
+func _unlock() -> void:
+	play_button.disabled = false
 
 func _on_button_pressed() -> void:
 	selected.emit(self.index)
--- scripts/level_menu.gd
+++ scripts/level_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var levels: Array[LevelInfo]
+var cards: Array
 @export var card_scene: PackedScene = preload("res://scenes/level_card.tscn")
 @onready var container: VBoxContainer = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer
 
@@ -10,9 +11,13 @@ func _ready() -> void:
 	if not status:
 		return
 	
-	status = render_levels()
+	var unlocked_levels: int = 10
+	status = render_levels(unlocked_levels)
+	
 	if not status:
 		return
+	
+
 
 
 func open_levels() -> bool:
@@ -29,15 +34,17 @@ func open_levels() -> bool:
 	levels.assign(unpacked_data)
 	return true
 
-func render_levels() -> bool:
+func render_levels(unlocked_levels: int) -> bool:
 	for child in container.get_children():
 		child.queue_free()
 	
 	for i in range(levels.size()):
 		var card = card_scene.instantiate()
 		container.add_child(card)
-		
+		cards.append(card)
 		card.setup(i, levels[i])
+		if i < unlocked_levels:
+			card._unlock()
 		card.selected.connect(_load_level)
 	
 	return true
